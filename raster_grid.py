59d5ac5740ce2d921bc43db4dfd1469761dba609
carme-hp <homspons@gmail.com>
2022-10-07 12:01:10 +0200

    make point a dataclass

--- raster_grid.py
+++ raster_grid.py
@@ -9,12 +9,10 @@
 from typing import Tuple
 from dataclasses import dataclass
 
+@dataclass
 class Point:
-    def __init__(self,
-                 x: float,
-                 y: float) -> None:
-        self.x = x
-        self.y = y
+    x: float
+    y: float
 
 class RasterGrid:
     @dataclass
